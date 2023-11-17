77e46f7c876d134d01d1bd8138ae8b5e8f431248
pszczepkowski <p.szczepkowski87@gmail.com>
2023-11-17 07:30:12 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  platform: 'bitbucket',
+  gitAuthor: 'Renovate Bot <botrenovate@gmail.com>',
+  username: 'bot-renovate',
+  onboarding: true,
+  onboardingConfig: {
+    extends: ['config:base']
+  },
+  repositories: [
+    'botrenovate/libria1'
+  ]
+}
